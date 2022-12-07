7497bcc0cf64fee49fafd502f362786859dd6abf
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-07 19:29:36 +0300

    Created the data models

---
+++ src/models/comment.model.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from 'mongoose';
+
+const CommentSchema = new Schema({
+	content: { type: String },
+	blogPost: { type: Schema.Types.ObjectId, ref: 'blogPost', required: true },
+	account: { type: Schema.Types.ObjectId, ref: 'account', required: true },
+	createdAt: { type: Date },
+	updatedAt: { type: Date },
+});
+
+export const Comment = model('comment', CommentSchema);
