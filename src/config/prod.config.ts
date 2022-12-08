701f881520c8d5dc257ddf919b1862b03b6cedb6
Nile Okomo <okomonile3@gmail.com>
2022-12-08 19:19:59 +0300

    Update missing directories

---
+++ src/config/prod.config.ts
@@ -0,0 +1,19 @@
+export default {
+  db: {
+    uri: process.env.DB_URI,
+  },
+  auth: {
+    jwt: {
+      secret: process.env.JWT_SECRET,
+      atExpires: parseInt(process.env.JWT_AT_EXPIRES), // expires after 30 seconds
+      rtExpires: parseInt(process.env.JWT_RT_EXPIRES), // expires after a day
+    },
+    admin: {
+      firstName: process.env.ADMIN_FIRST_NAME,
+      lastName: process.env.ADMIN_LAST_NAME,
+      email: process.env.ADMIN_EMAIL,
+      phone: process.env.ADMIN_PHONE,
+      password: process.env.ADMIN_PASSWORD,
+    },
+  },
+};
