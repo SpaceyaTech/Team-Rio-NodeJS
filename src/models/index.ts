a7ecfb24e0a178c03157e4447b9fb861d75ab8dc
Nile Okomo <okomonile3@gmail.com>
2022-12-08 18:51:52 +0300

    Prettier fix

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,11 +5,11 @@ import config from '../config';
 const { db } = config();
 
 export const dbSetup = () => {
-	mongoose.set('strictQuery', false);
-	mongoose.connect(db.uri, (err) => {
-		if (err) console.error('Error:', err);
-		console.info('Info:', 'Connected to database');
-	});
+  mongoose.set('strictQuery', false);
+  mongoose.connect(db.uri, (err) => {
+    if (err) console.error('Error:', err);
+    console.info('Info:', 'Connected to database');
+  });
 };
 
 export * from './account.model';
--- src/models/role.model.ts
+++ src/models/role.model.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from 'mongoose';
 
 const RoleSchema = new Schema({
-	name: { type: String },
-	createdAt: { type: Date },
-	updatedAt: { type: Date },
+  name: { type: String },
+  createdAt: { type: Date },
+  updatedAt: { type: Date },
 });
 
 export const Role = model('role', RoleSchema);
